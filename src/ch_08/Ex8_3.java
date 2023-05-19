3c45b46d7b7d5f5ae2ab92074d4e78e34ed93631
최정필 <wjdvlf1407@gmail.com>
2023-05-19 19:07:16 +0900

    11

--- src/ch_08/Ex8_3.java
+++ src/ch_08/Ex8_3.java
@@ -23,6 +23,7 @@ public class Ex8_3 {
 		
 		LQ.printQueue();
 		
+		
 		LQ.enQueue('F');
 		LQ.enQueue('G');
 		LQ.enQueue('H');
@@ -39,7 +40,19 @@ public class Ex8_3 {
 //		
 //		이어서 10번의 enQueue 후에 한번의 printQueue를 실행하여, 연결큐는 데이터를 많이 저장하더라도 
 //		필요한 공간을 할당받아서 계속 큐에 많은 데이터를 저장할 수 있음을 확인하시오. 
-//
+		LQ.enQueue('K');
+		LQ.enQueue('L');
+		LQ.enQueue('M');
+		LQ.enQueue('N');
+		LQ.enQueue('O');
+		
+		LQ.enQueue('P');
+		LQ.enQueue('Q');
+		LQ.enQueue('R');
+		LQ.enQueue('S');
+		LQ.enQueue('T');
+		
+		LQ.printQueue();
 //		위 실습내용의 결과 화면을 캡처하여 실습 과제물로 제출하시오.  
 //
 	}
