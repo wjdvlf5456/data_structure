c7e6950096eb6be2e3090337ba121a3c4c655f55
최정필 <wjdvlf1407@gmail.com>
2023-05-22 13:46:15 +0900

    이진 트리

--- src/ch_09/Ex9_1.java
+++ src/ch_09/Ex9_1.java
@@ -3,9 +3,30 @@ package ch_09;
 public class Ex9_1 {
 
 	public static void main(String[] args) {
-		
+
 		LinkedTree T = new LinkedTree();
+
+		TreeNode n11 = T.makeBT(null, 'K', null);
+		TreeNode n10 = T.makeBT(null, 'F', null);
+		TreeNode n9 = T.makeBT(null, 'J', null);
+		TreeNode n8 = T.makeBT(null, 'I', null);
+		TreeNode n7 = T.makeBT(null, 'H', null);
+		TreeNode n6 = T.makeBT(null, 'G', n11);
+		TreeNode n5 = T.makeBT(n8, 'E', n9);
+		TreeNode n4 = T.makeBT(n7, 'D', null);
+		TreeNode n3 = T.makeBT(n10, 'C', n6);
+		TreeNode n2 = T.makeBT(n4, 'B', n5);
+		TreeNode n1 = T.makeBT(n2, 'A', n3);
 		
-	}
+		System.out.print("Preorder : ");
+		T.preorder(n1);
+		
+		System.out.print("\nInorder : ");
+		T.inorder(n1);
 	
+		System.out.print("\nPostorder : ");
+		T.postorder(n1);
+
+	}
+
 }
--- src/ch_09/LinkedTree.java
+++ src/ch_09/LinkedTree.java
@@ -1,13 +1,44 @@
 package ch_09;
 
 public class LinkedTree {
-	
+
 	private TreeNode root;
-	
+
 	public TreeNode makeBT(TreeNode bt1, Object data, TreeNode bt2) {
 		TreeNode root = new TreeNode();
-		
-		
+		root.data = data;
+		root.left = bt1;
+		root.right = bt2;
+
+		return root;
+	}
+
+	public void preorder(TreeNode root) {
+		if (root != null) {
+			System.out.print(root.data);
+			preorder(root.left);
+			preorder(root.right);
+		}
+
+	}
+
+	public void inorder(TreeNode root) {
+		if (root != null) {
+			inorder(root.left);
+			System.out.print(root.data);
+			inorder(root.right);
+		}
+
+	}
+
+	public void postorder(TreeNode root) {
+		if (root != null) {
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.data);
+
+		}
+
 	}
 
 }
