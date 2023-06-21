ba2085cf9c93c7b151d04aba313db58a1261036d
최정필 <wjdvlf1407@gmail.com>
2023-06-21 13:18:02 +0900

    병합 정렬 프로그램 실습

--- src/ch_11_merge/Ex11_6.java
+++ src/ch_11_merge/Ex11_6.java
@@ -1,14 +1,15 @@
-package ch_11_bubble;
+package ch_11_merge;
 
-public class Ex11_2 {
+public class Ex11_6 {
 	public static void main(String args[]) {
-		int a[] = { 20, 15, 30, 5, 25, 70, 40, 55, 10, 35};
+		int a[] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+		int size = a.length;
 		Sort S = new Sort();
 		System.out.printf("\n정렬할 원소 : ");
 		for (int i = 0; i < a.length; i++)
 			System.out.printf(" %d", a[i]);
 		System.out.println();
-		S.bubbleSort(a);
+		S.mergeSort(a, 0, size - 1);
 	}
 
 }
